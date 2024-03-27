2bb55951609730138aa6ba1c9233eee7b85d7523
Kirill <k187m18733@gmail.com>
2024-03-27 22:49:48 +0300

    feat: solve getAllResult

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -125,8 +125,9 @@ async function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  const result = promises.map((promise) => promise.catch(() => null));
+  return Promise.all(result);
 }
 
 /**
